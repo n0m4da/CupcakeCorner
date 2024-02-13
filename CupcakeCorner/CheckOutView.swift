512731def13523e6a769fc5c2b9971bcd379305f
n0m4da <nomad._@hotmail.com>
2024-02-13 13:35:04 -0700

    check order, and api put example

---
+++ CupcakeCorner/CheckOutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckOutView.swift
+//  CupcakeCorner
+//
+//  Created by Luis Rivera on 07/02/24.
+//
+
+import SwiftUI
+
+struct CheckOutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckOutView()
+}
